bbd86705d3acc4d5b02336e13b76febe16877b8b
gbhn4567x <gbhn4567x@yahoo.com.tw>
2019-05-12 08:55:37 +0800

    Create csv_merge.py

---
+++ csv_merge.py
@@ -0,0 +1,8 @@
+import glob
+import codecs
+read_files=glob.glob(r'*.csv')# 通過glob返回滿足條件的文件列表,也可以用os.listdir獲取當前路徑下的文件
+
+with codecs.open('output.csv', 'ab+') as outfile:
+    for f in read_files:
+        with open(f,'rb') as infile:
+            outfile.write(infile.read())
